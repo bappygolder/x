7c675e7368e16f5793d6773cf24fb8f24e053b0a
Bappy <bups.co@gmail.com>
2023-08-21 10:26:08 +1000

    Added toggle option for Content Type & CTA

--- script.js
+++ script.js
@@ -1,67 +1,82 @@
 var callToActionPillars = [
-    "Join Our Newsletter",
-    "Join oLab Team",
-    "Join Our Consulting Program",
-    "Hire Our Team For Your Business",
-    "Get Our Agency Service",
-    "Partner With oLab (Rev. Share Partnership)",
+  "Join Our Newsletter",
+  "Join oLab Team",
+  "Join Our Consulting Program",
+  "Hire Our Team For Your Business",
+  "Get Our Agency Service",
+  "Partner With oLab (Rev. Share Partnership)",
 ];
 
 var generalPillars = [
-    "Bappy’s Philosophy or Tenets",
-    "Business Building & Entrepreneurship",
-    "Remote Work and Leadership",
-    "Freelancing & Being A Remote Team Member",
-    "Inspirational & Motivational",
-    "Technology & Automation",
-    "Ongoing Learning & Growth",
-    "Personal Development",
-    "Sales, Marketing & Business Growth",
-    "Other Account Mention",
-    "Personal Branding",
-    "Growth in 𝕏",
-    "Philosophy",
-    "Taking Action",
+  "Bappy’s Philosophy or Tenets",
+  "Business Building & Entrepreneurship",
+  "Remote Work and Leadership",
+  "Freelancing & Being A Remote Team Member",
+  "Inspirational & Motivational",
+  "Technology & Automation",
+  "Ongoing Learning & Growth",
+  "Personal Development",
+  "Sales, Marketing & Business Growth",
+  "Other Account Mention",
+  "Personal Branding",
+  "Growth in 𝕏",
+  "Philosophy",
+  "Taking Action",
 ];
 
 var contentTypes = [
-    "Small Posts (Tweets)",
-    "Long Posts (Article & Similar)",
-    "Question",
-    "Poll",
-    "Voice Tweet",
-    "Video: Short",
-    "Video: Long",
-    "Audio Recordings (Podcasts, Space Recording etc.)",
-    "Quote Repost",
-    "Photo / Gif",
+  "Small Posts (Tweets)",
+  "Long Posts (Article & Similar)",
+  "Question",
+  "Poll",
+  "Voice Tweet",
+  "Video: Short",
+  "Video: Long",
+  "Audio Recordings (Podcasts, Space Recording etc.)",
+  "Quote Repost",
+  "Photo / Gif",
 ];
 
 var currentCombination = 0;
 
-function next() {
-    var nextGeneral = generalPillars[currentCombination % generalPillars.length];
-    var nextContent = contentTypes[currentCombination % contentTypes.length];
-    var nextCTA = callToActionPillars[currentCombination % callToActionPillars.length];
+// Function to handle the toggle action
+function handleToggle() {
+  // Reference to the checkboxes
+  var contentTypeToggle = document.getElementById('contentTypeToggle');
+  var ctaToggle = document.getElementById('ctaToggle');
 
-    var output = `<div class="row">
-                    <div class="title"><strong>Next Tweet:</strong></div>
-                    <div class="content">${nextGeneral}</div>
-                  </div>
-                  <div class="row">
-                    <div class="title"><strong>Content Type:</strong></div>
-                    <div class="content">${nextContent}</div>
-                  </div>
-                  <div class="row">
-                    <div class="title"><strong>Possible CTA:</strong></div>
-                    <div class="content">${nextCTA}</div>
-                  </div>`;
-    
-    document.getElementById('output').innerHTML = output;
+  var nextGeneral = generalPillars[currentCombination % generalPillars.length];
+  var nextContent = contentTypeToggle.checked ? contentTypes[currentCombination % contentTypes.length] : null;
+  var nextCTA = ctaToggle.checked ? callToActionPillars[currentCombination % callToActionPillars.length] : null;
 
-    currentCombination++;
+  var output = `<div class="row">
+                  <div class="title"><strong>Next Post:</strong></div>
+                  <div class="content">${nextGeneral}</div>
+                </div>`;
+  if (nextContent) {
+      output += `<div class="row content-type-row">
+                  <div class="title"><strong>Content Type:</strong></div>
+                  <div class="content">${nextContent}</div>
+                 </div>`;
+  }
+  if (nextCTA) {
+      output += `<div class="row cta-row">
+                  <div class="title"><strong>Possible CTA:</strong></div>
+                  <div class="content">${nextCTA}</div>
+                 </div>`;
+  }
+  
+  document.getElementById('suggestedContent').style.display = 'none'; // Hide the placeholder text
+  document.getElementById('generatedContent').innerHTML = output;
+  document.getElementById('generatedContent').style.display = 'block'; // Show the generated content
+  currentCombination++;
 }
 
 function displayNext() {
-    next();
+  document.getElementById('suggestedContent').style.display = 'none'; // Hide the placeholder text
+  handleToggle();
 }
+
+// Event listeners for the checkboxes
+document.getElementById('contentTypeToggle').addEventListener('change', handleToggle);
+document.getElementById('ctaToggle').addEventListener('change', handleToggle);
